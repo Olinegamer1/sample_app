9cf8b37ccf53a3c7e39727d66fa3d4ca01cda4bd
Pavel Lysenkov <pavel.lysenkov@saltpepper.ru>
2023-12-27 01:01:42 +0300

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   validates :name,
             presence: true,
             length:   { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,
             presence:   true,
             length:     { maximum: 255 },
